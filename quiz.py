bfa69f2cf9b2d8c0872f3403bc834931f79d6a1e
EMEKS <Chukwuemeka.collins@binghamuni.edu.ng>
2026-03-28 10:39:33 +0800

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,69 +1,62 @@
-
 print("Welcome to my computer quiz!")
 
-playing= input("Do you want to play? ")
+playing = input("Do you want to play? ")
 
 if playing.lower() != "yes":
-     quit()
+    quit()
 
-print("okay! Lets play:")
+print("Okay! Let's play:")
 
-score= 0
+score = 0
 
-answer= input("What does RAM stand for ?")
-if answer.lower()== "random access memory":
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
     print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
-answer = input("What does CPU stand for ? ")
+answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
- print("Correct!")
- score += 1
-else:
- print("Incorrect!")
-
-answer = input("What does USB stand for ? ")
-if answer.lower() == "universal serial bus":
- print("Correct!")
- score += 1
-else:
- print("Incorrect!")
-
-answer = input("What does HTML stand for ? ")
-if answer.lower() == "hyper text markup language":
- print("Correct!")
-
- score += 1
-
-else:
- print("Incorrect!")
-
-answer = input("What does GPU stand for ? ")
-if answer.lower() == "graphics processing unit":
-  print("Correct!")
-
-  score += 1
-else:
-  print("Incorrect!")
-
-answer = input("What does ROM stand for ?")
-if answer.lower() == "read only memory":
- print("Correct!")
-
- score += 1
-else:
-  print("Incorrect!")
-
-answer = input("What does PSU stand for ? ")
-if answer.lower() == "power supply unit":
- print("Correct!")
-
- score += 1
+    print("Correct!")
+    score += 1
 else:
     print("Incorrect!")
 
-print("You got" + str(score) + "questions correct!")
-print("You got" + str((score/6) * 100) + "%")
+answer = input("What does USB stand for? ")
+if answer.lower() == "universal serial bus":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
 
+answer = input("What does HTML stand for? ")
+if answer.lower() == "hyper text markup language":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does ROM stand for? ")
+if answer.lower() == "read only memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str(round((score / 7) * 100, 1)) + "%")
